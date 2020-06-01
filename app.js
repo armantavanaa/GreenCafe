ef0ed0ada67fe3f05394e4927536f94e40566cd8
armantavanaa <58196629+armantavanaa@users.noreply.github.com>
2020-06-01 11:34:16 -0400

    world

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(request, response) {
-  response.send('hello');
+  response.send('hello world');
 });
 
 app.listen(3000);
